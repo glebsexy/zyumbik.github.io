440cdbc341d6d693dc88a8275326135f186fe315
zyumbik <gleb.sabirzyanov@gmail.com>
2016-07-27 10:47:47 +1000

    Borders on title click

--- scripts/firstscript.js
+++ scripts/firstscript.js
@@ -3,4 +3,22 @@ window.onload = function() {
 	button.onclick = function() {
 		document.getElementById("boringarticle").getElementsByTagName("header")[0].getElementsByTagName("h3")[0].innerHTML = "Fun article";
 	}
+	document.getElementsByTagName("header")[0].getElementsByTagName("h1")[0].getElementsByTagName("a")[0].onclick = function() {
+		var div = document.getElementsByTagName("div");
+		var header = document.getElementsByTagName("header");
+		var a = document.getElementsByTagName("a");
+		var article = document.getElementsByTagName("article");
+		for (var i = 0; i < div.length; i++) {
+			div[i].style.border = "1px solid red";
+		}
+		for (var i = header.length - 1; i >= 0; i--) {
+			header[i].style.border = "1px solid blue";
+		}
+		for (var i = a.length - 1; i >= 0; i--) {
+			a[i].style.border = "1px solid black";
+		}
+		for (var i = article.length - 1; i >= 0; i--) {
+			article[i].style.border = "1px solid green";
+		}
+	}
 }
