a35fc19be111a6d96fff4fc8b5e5ff4c3b93a0ae
zyumbik <gleb.sabirzyanov@gmail.com>
2016-07-30 10:38:47 +1000

    Add circle on click

--- pages/scripts/tic-tac-toe.js
+++ pages/scripts/tic-tac-toe.js
@@ -1,9 +1,21 @@
 window.onload = function() {
 	// var cells = document.getElementsByClassName("cell");
+
+	// Page reload indicator
+	document.getElementById("game").style = "background-color: #ffffff;";
+
 	var cells = document.getElementsByClassName("cell");
+
+	// var cross = "<div class=\"cross game-element\"></div>";
+	// var circle = "<div class=\"circle game-element\"></div>";
+
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].onclick = function() {
-			alert(this.id);
+			var cell = document.getElementById(this.id);
+			var gameElement = document.createElement("div");
+			gameElement.className = "circle game-element";
+			cell.appendChild(gameElement);
 		};
 	};
+
 }
