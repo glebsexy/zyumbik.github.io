a47020b1725414378a8763c7a28cfabf3663186d
zyumbik <gleb.sabirzyanov@gmail.com>
2016-08-12 15:53:10 +1000

    Infinitely loading photos in recursive onload method

--- pages/scripts/photo-tiles.js
+++ pages/scripts/photo-tiles.js
@@ -2,9 +2,7 @@
 
 window.onload = function() {
 	// var columns = document.getElementsByClassName("column");
-	for (var i = 3 - 1; i >= 0; i--) {
-		addPhoto(document.getElementsByClassName("photos")[0]);
-	}
+	addPhoto(document.getElementsByClassName("photos")[0]);
 	document.getElementsByClassName("photos")[0].onclick = function() {
 		addPhoto(this);
 	};
@@ -13,10 +11,12 @@ window.onload = function() {
 var counter = 0;
 
 function addPhoto (container) {
-	// container.innerHTML += "<img src=\"https://new.vk.com/images/emoji/D83DDE01.png\" alt=\"Photo\" class=\"photo\">";
 	var img = document.createElement("img");
 	img.src = "https://source.unsplash.com/random/" + counter++;
-	container.appendChild(img);
+	img.onload = function() {
+		addPhoto(container);
+		container.appendChild(img);
+	};
 }
 
 function changeMenuIcon(z){
