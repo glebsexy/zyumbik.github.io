3d66e896091b02afc5a957b09af32a5a7ce606af
zyumbik <gleb.sabirzyanov@gmail.com>
2016-08-12 15:27:16 +1000

    Add photos with cache breaker

--- pages/scripts/photo-tiles.js
+++ pages/scripts/photo-tiles.js
@@ -1,3 +1,24 @@
+"use strict";
+
+window.onload = function() {
+	// var columns = document.getElementsByClassName("column");
+	for (var i = 3 - 1; i >= 0; i--) {
+		addPhoto(document.getElementsByClassName("photos")[0]);
+	}
+	document.getElementsByClassName("photos")[0].onclick = function() {
+		addPhoto(this);
+	};
+};
+
+var counter = 0;
+
+function addPhoto (container) {
+	// container.innerHTML += "<img src=\"https://new.vk.com/images/emoji/D83DDE01.png\" alt=\"Photo\" class=\"photo\">";
+	var img = document.createElement("img");
+	img.src = "https://source.unsplash.com/random/" + counter++;
+	container.appendChild(img);
+}
+
 function changeMenuIcon(z){
 	z.classList.toggle("change");
 }
