f18f9c78a64369610c91b098b119e221b215bc50
zyumbik <gleb.sabirzyanov@gmail.com>
2017-02-15 18:59:35 +1000

    Valentines's day page

--- pages/scripts/photo-tiles.js
+++ pages/scripts/photo-tiles.js
@@ -2,21 +2,26 @@ window.onload = function() {
 	addPhoto(document.getElementsByClassName("photos")[0]);
 };
 
+document.onload = function() {
+	document.getElementById("gotoStart").onclick = scrollToTop();
+	document.getElementById("gotoEnd").onclick = scrollToEnd();
+};
+
 var counter = 0, maxElements = 10;
 var column;
 
 function addPhoto (container) {
-	if (counter < maxElements) {
-		var div = document.createElement("div");
-		div.className = "image";
-		var img = new Image();
-		img.src = "https://source.unsplash.com/random/" + (+new Date());
-		img.ondragstart = function(event) {
-			event.preventDefault();
-		};
-		img.onload = function() {
+	var div = document.createElement("div");
+	div.className = "image";
+	var img = new Image();
+	img.src = "https://source.unsplash.com/random/" + (+new Date());
+	img.ondragstart = function(event) {
+		event.preventDefault();
+	};
+	img.onload = function() {
+		if (counter < maxElements) {
 			addPhoto(container);
-			if (img.naturalHeight < img.naturalWidth && counter < maxElements) {
+			if (img.naturalHeight < img.naturalWidth) {
 				counter++;
 				div.appendChild(img);
 				if (counter % 2 !== 0) {
@@ -32,7 +37,7 @@ function addPhoto (container) {
 	}
 }
 
-function changeMenuIcon (z){
+function changeMenuIcon(z) {
 	z.classList.toggle("change");
 	document.getElementById("menu").classList.toggle("hidden");
 }
@@ -40,10 +45,18 @@ function changeMenuIcon (z){
 var curYPos = 0, curXPos = 0, curDown = false;
 
 window.addEventListener('mousemove', function(e){ 
-	if(curDown === true){
+	if (curDown === true) {
 		window.scrollBy(curXPos - e.pageX, curYPos - e.pageY);
 	}
 });
 
 window.addEventListener('mousedown', function(e){ curDown = true; curYPos = e.pageY; curXPos = e.pageX; });
 window.addEventListener('mouseup', function(e){ curDown = false; });
+
+function scrollToTop () {
+	window.scrollTo(0, 0);
+}
+
+function scrollToEnd () {
+	window.scrollTo(window.scrollWidth, window.scrollHeight);
+}
